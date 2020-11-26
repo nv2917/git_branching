d297b10349ff5c33369560d84b5f342c217ae43a
nv2917 <nv2917@ic.ac.uk>
2020-11-26 11:08:52 +0000

    ready

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,10 @@ public class Main {
         for(int i=1;i<=10;i++) {
             System.out.println(i);
         }
+        int sum = 0;
+        for(int i=30;i<=40;i++) {
+            sum = sum+i;
+        }
+        System.out.println(sum);
     }
 }
